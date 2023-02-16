e169168dfd658e804ea15e7092ed5c0be967b800
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-16 23:17:51 +0300

    fix typo

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,12 @@ const Title = require("../models/Title");
 
 const index = async (req, res, next) =>
 {
+    const daily = await Title.find({isVisible:true, createdAt:new Date().toDateString()});
     Title.findRandom({isVisible:true}, {}, {limit:10},function(err, randomTitles)
     {
         if(err) return next(err);
         else{
-            res.status(200).json({success:true, data:randomTitles});
+            res.status(200).json({success:true, data:randomTitles, daily:daily});
         }
     });
 }
